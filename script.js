ceab75ae311a208f48c8250d461e9ab7f3b55923
TinekeKvdW <84176956+TinekeKvdW@users.noreply.github.com>
2021-06-04 11:35:56 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const age = number = 17;
+
+if (age > 17) {
+
+    document.write("U bent 18 jaar of ouder.");
+
+} else if (age < 17) {
+
+    document.write("U bent nog geen 18 jaar en daarom heeft u helaas geen toegang.");
+
+}
+
+console.log("10");
+
+
+const isFemale = true;
+
+if (isFemale) {
+
+    document.write("U bent vrouw, u heeft toegang.");
+
+} else {
+
+    document.write("U bent man, u heeft geen toegang.");
+}
+
+console.log("isFemale");
+
+const driverStatus = "bob";
+
+if (driverStatus = bob) {
+
+    document.write("U kunt zelf naar huis rijden")
+
+} else {
+
+    document.write("U kunt niet zelf naar huis rijden")
+
+}
+
+console.log("driverStastus");
